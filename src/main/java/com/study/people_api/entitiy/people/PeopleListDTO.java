bae04db97c1489ae81cec3a1dffbbe86574e493a
HanselVinicius <hanelvinicius@gmail.com>
2023-07-28 19:45:19 -0300

    telefone on getAll

--- src/main/java/com/study/people_api/entitiy/people/PeopleListDTO.java
+++ src/main/java/com/study/people_api/entitiy/people/PeopleListDTO.java
@@ -4,10 +4,10 @@ import com.study.people_api.entitiy.address.Address;
 
 import java.util.Date;
 
-public record PeopleListDTO(Long id, String nome, String cpf, Date data_de_aniversario, Address address) {
+public record PeopleListDTO(Long id, String nome, String cpf, Date data_de_aniversario, Address address,String telefone) {
 
 
     public PeopleListDTO(People people){
-        this(people.getId(), people.getNome(), people.getCpf(), people.getDataDeAniversario(),people.getEndereco());
+        this(people.getId(), people.getNome(), people.getCpf(), people.getDataDeAniversario(),people.getEndereco(),people.getTelefone());
     }
 }
